9ebed6a9958cd2e7d4f2e59de6816ef568d3da35
Test Testington <Test@Testmail.com>
2023-02-14 20:15:56 +0200

    Further developed store class

--- Portfolio_project_CS101.py
+++ Portfolio_project_CS101.py
@@ -4,13 +4,31 @@ os.system('cls')
 #Defining main store class
 class Store:
     #Starting stock, budget and reputation
-    current_stock = [];
-    store_budget = 0;
+    current_stock = {"Pistol":0, "Shotgun":0, "Machine gun":0, "Sub-machine gun":0, "Hunting knife":0};
+    store_budget = 2000;
     reputation = 0;
     #Initializing new store
     def __init__(self, store_name) -> None:
         self.store_name =store_name;
     #Defining method for repleneshing stock
+    def add_to_stock(self) -> None:
+        exit_store = 0;
+        while exit_store == 0:
+            add_to_stock_action = input("(Buy/Exit)?");
+            if add_to_stock_action.lower() == "buy":
+                print("Your current budget is: " + str(Store.store_budget) + ". What would you like to buy?");
+                add_to_stock_buy = input();
+            elif add_to_stock_action.lower() == "exit":
+                while exit_store == 0:
+                    exit_store_check = input("Are you sure you want to exit? (y/n)")
+                    if exit_store_check.lower() == "y":
+                        exit_store = 1;
+                        print("See you later, call anytime")
+                    elif exit_store_check.lower() == "n":
+                        break
+                    else:
+                        pass
+            
     #Defining method for checking store name, stock, reputation
     #Defining method for setting gun prices
     #Defining method for checking store budget, income, profit
@@ -22,7 +40,7 @@ class Supplier:
     def __init__(self) -> None:
         pass
     #Defining a method for checking stock
-    def check_stock(self):
+    def check_stock(self) -> None:
         for key, value in Supplier.supplier_stock.items():
             print(str(value) + " × " + key + " for " + str(Market.market_prices[key]) + " each")
 
@@ -39,7 +57,7 @@ class Market:
     market_prices = {"Pistol":200.0, "Shotgun":300.0, "Machine gun":1000.0, "Sub-machine gun":700.0, "Hunting knife":100.0};
     def __init__(self) -> None:
         pass
-    def change_all_prices(self, change):
+    def change_all_prices(self, change) -> None:
         for key, value in Market.market_prices.items():
             Market.market_prices[key] = value + (value/100*change);
 
@@ -56,5 +74,5 @@ continue_dialog = input("'Enter to continue'");
 print("Ahh, what the hell. I'll make you a discount, 10% off, for old times sake. Just this time though")
 gun_market = Market();
 gun_market.change_all_prices(-10);
-gun_supplier.check_stock()
-
+gun_supplier.check_stock();
+new_store.add_to_stock();
