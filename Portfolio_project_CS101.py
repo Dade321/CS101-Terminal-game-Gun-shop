09de3207195deaa7be359cda2cde0cb98b987057
Test Testington <Test@Testmail.com>
2023-02-14 21:18:05 +0200

    Add_to_stock method in Store class

--- Portfolio_project_CS101.py
+++ Portfolio_project_CS101.py
@@ -11,13 +11,34 @@ class Store:
     def __init__(self, store_name) -> None:
         self.store_name =store_name;
     #Defining method for repleneshing stock
-    def add_to_stock(self) -> None:
+    def add_to_stock(self, market_prices) -> None:
         exit_store = 0;
         while exit_store == 0:
             add_to_stock_action = input("(Buy/Exit)?");
             if add_to_stock_action.lower() == "buy":
                 print("Your current budget is: " + str(Store.store_budget) + ". What would you like to buy?");
                 add_to_stock_buy = input();
+                try:
+                    gun_cost = market_prices[add_to_stock_buy.capitalize()];
+                except Exception:
+                    print("Sorry I don't have this gun.")
+                else:
+                    affordable_amount = int((Store.store_budget - Store.store_budget % gun_cost)/gun_cost);
+                    print("One " + str(add_to_stock_buy.capitalize()) + " costs " + str(gun_cost) + ". With your current budget you can buy " + str(affordable_amount) + ". How much do you need?");
+                    amount_to_buy = input();
+                    try:
+                       int(amount_to_buy) + 1;
+                    except Exception:
+                        print("Enter an amount") ;
+                    else:
+                        if int(amount_to_buy) > affordable_amount:
+                            print("Sorry you can't afford that much.")
+                        else:
+                            Store.store_budget -= int(amount_to_buy) * gun_cost;
+                            Store.current_stock[add_to_stock_buy.capitalize()] += int(amount_to_buy);
+                            print("You now have:")
+                            for key, value in Store.current_stock.items():
+                                print(str(value) + " × " + key)
             elif add_to_stock_action.lower() == "exit":
                 while exit_store == 0:
                     exit_store_check = input("Are you sure you want to exit? (y/n)")
@@ -75,4 +96,4 @@ print("Ahh, what the hell. I'll make you a discount, 10% off, for old times sake
 gun_market = Market();
 gun_market.change_all_prices(-10);
 gun_supplier.check_stock();
-new_store.add_to_stock();
+new_store.add_to_stock(gun_market.market_prices);
